9d2334eecfd55454d6df938bfbd46fa507a41182
Denis <denismunene999@gmail.com>
2022-09-01 15:33:22 +0300

    Done.

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,13 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def recieved_one? (item_name)
+    self.freebies.each do |freebie| 
+      if freebie.item_name == item_name
+        return true
+      end  
+    end
+    false  
+  end  
 end
